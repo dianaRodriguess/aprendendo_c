c64c5dfd8482efe416e4a5687a7d3fa1e9358c0e
dianaRodriguess <dianarodrigues044@gmail.com>
2024-05-18 10:47:08 -0300

    laços while, for e do-while

---
+++ livro/lacosr.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main(void){
+    // WHILE
+    // int n;
+    // int i;
+    // int f = 1;
+
+    // printf("Digite um número inteiro não negativo: ");
+    // scanf("%d", &n);
+
+    // i = 1;
+
+    // while (i <= n) {
+    //     f *= i;
+    //     i++;
+    // }
+
+    // printf("O fatoria de %d é: %d \n", n, f);
+    // // printf("%d", i);
+
+
+    // FOR
+    // int n;
+    // int f = 1;
+
+    // printf("Digite um número inteiro não negativo: ");
+    // scanf("%d", &n);
+
+    // for (int i = 1; i <= n; i++){
+    //     f *= i;
+    //     // i++;
+    // }
+    
+    // printf("O fatorial de %d é: %d \n", n, f);
+
+    // DO-WHILE
+
+    // int n;
+    // int f = 1;
+    
+    // do {
+    //     printf("Digite um número inteiro não negativo: ");
+    //     scanf("%d", &n);
+    // }while(n < 0);
+
+    // for (int i = 1; i <= n; i++){
+    //     f *= i;
+    // }
+    // printf("Fatorial de %d é: %d \n", n, f);
+
+
+    // BRAKE CONTINUE
+
+    // for (int i = 0; i < 10; i++){
+    //     if (i == 5){
+    //         break;
+    //     }
+    //     printf(" %d ", i);
+    // }
+    // printf("\n");
+    // for (int i = 0; i < 10; i++){
+    //     if (i == 5){
+    //         continue;
+    //     }
+    //     printf(" %d ", i);
+    // }
+    // printf("\n");
+
+    return 0;
+}
